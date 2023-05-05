6ce95cd0e52bcb284583dba6e423b9560d73de06
Darin Kuntze <dkuntze@gmail.com>
2023-05-05 16:15:40 -0500

    wip gradient card

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -5,13 +5,24 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    const wDiv = document.createElement('div');
+
+    wDiv.classList.add('card-container');
     li.innerHTML = row.innerHTML;
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
+    wDiv.innerHTML = li.innerHTML;
+    if (block.classList.contains('gradient-shadow')) {
+      const xDiv = document.createElement('div');
+      xDiv.classList.add('card-extrusion');
+      wDiv.prepend(xDiv);
+    }
+    li.innerHTML = wDiv.outerHTML;
     ul.append(li);
   });
+
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
