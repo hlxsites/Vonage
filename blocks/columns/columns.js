aa2f940cb523b4b5ebc84169eb7990f7a52e06af
Darin Kuntze <dkuntze@gmail.com>
2023-06-01 14:44:19 -0500

    rc attempt one

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,3 +1,25 @@
+function addSlimPromoClick(block, mq) {
+  if (mq.matches) {
+    block.querySelector('div.slim-promo.columns-2-cols > div > div:nth-child(2)').addEventListener('click', () => {
+      const pBlock = block.querySelector('div.slim-promo.columns-2-cols p');
+      const pButton = block.querySelector('.slim-promo a.button.secondary');
+      const pH2 = block.querySelector('div.slim-promo.columns-2-cols h2');
+      // toggle the paragraph and the button display
+      if (pBlock.style.display === 'block') {
+        pBlock.style.display = 'none';
+        pButton.style.display = 'block';
+        pH2.style.fontSize = 'initial';
+      } else {
+        pBlock.style.display = 'block';
+        pButton.style.display = 'none';
+        pH2.style.fontSize = '2.4rem'; // enlarge the h2 font size
+      }
+    });
+  } else {
+    block.querySelector('div.slim-promo.columns-2-cols > div > div:nth-child(2)').removeEventListener('click');
+  }
+}
+
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -24,5 +46,7 @@ export default function decorate(block) {
     vo.classList.add('view-offer', 'button', 'secondary');
     bc.append(vo);
     block.querySelector('h2').insertAdjacentElement('afterend', bc);
+    const mq = window.matchMedia('(max-width: 767px)');
+    addSlimPromoClick(block, mq);
   }
 }
