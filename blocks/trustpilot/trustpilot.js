be057d29087da29f6063bd62a706dfc6880d191d
Rob Rusher <robrusher@gmail.com>
2023-06-07 16:53:08 -0600

    add tp popup

--- blocks/trustpilot/trustpilot.js
+++ blocks/trustpilot/trustpilot.js
@@ -8,7 +8,12 @@ export default function decorate(block) {
   tp.dataset.locale = 'en-US';
   tp.dataset.templateId = '5406e65db0d04a09e042d5fc';
   tp.dataset.businessunitId = '481735560000640005026e18';
+  tp.dataset.styleHeight = '30px';
+  tp.dataset.styleWidth = '100%';
   tp.dataset.theme = 'light';
+  tp.dataset.stars = '4,5';
+  tp.dataset.reviewLanguage = 'en';
+  tp.style = 'position: relative;';
   if (cfg.type === 'narrow') {
     const tpiframe = document.createElement('iframe');
     tpiframe.title = 'Customer reviews powered by Trustpilot';
@@ -16,6 +21,12 @@ export default function decorate(block) {
       tpiframe.src = cfg.src;
       tp.append(tpiframe);
     }
+  }
+  if (cfg.popup) {
+    const tpPopupiframe = document.createElement('iframe');
+    tpPopupiframe.title = 'Customer reviews powered by Trustpilot';
+    tpPopupiframe.src = cfg.popup;
+    tp.append(tpPopupiframe);
   }
   block.append(tp);
   decorateIcons(block);
