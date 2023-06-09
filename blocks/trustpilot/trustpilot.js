aca9dd82fbe4057a16f1d5ac608a155bb0b713e9
Rob Rusher <robrusher@gmail.com>
2023-06-09 16:52:45 -0600

    linting

--- blocks/trustpilot/trustpilot.js
+++ blocks/trustpilot/trustpilot.js
@@ -14,27 +14,48 @@ export default function decorate(block) {
   tp.dataset.businessunitId = '481735560000640005026e18';
 
   // Required block argument
-  if (cfg.template) { tp.dataset.templateId = cfg.template; }
-  else { console.warn('Missing the required TrustPilot templateId'); }
+  try {
+    tp.dataset.templateId = cfg.template;
+  } catch {
+    console.warn('Missing the required TrustPilot templateId');
+  }
 
   // Optional block arguments
-  if (cfg.height) { tp.dataset.styleHeight = cfg.height; }
-  else { tp.dataset.styleHeight = '30px'; }
+  if (cfg.height) {
+    tp.dataset.styleHeight = cfg.height;
+  } else {
+    tp.dataset.styleHeight = '30px';
+  }
 
-  if (cfg.width) { tp.dataset.styleWidth = cfg.width; }
-  else { tp.dataset.styleWidth = '100%'; }
+  if (cfg.width) {
+    tp.dataset.styleWidth = cfg.width;
+  } else {
+    tp.dataset.styleWidth = '100%';
+  }
 
-  if (cfg.theme) { tp.dataset.theme = cfg.theme; }
-  { tp.dataset.theme = 'light'; }
+  if (cfg.theme) {
+    tp.dataset.theme = cfg.theme;
+  } else {
+    tp.dataset.theme = 'light';
+  }
 
-  if (cfg.stars) { tp.dataset.stars = cfg.stars; }
-  else { tp.dataset.stars = '4,5'; }
+  if (cfg.stars) {
+    tp.dataset.stars = cfg.stars;
+  } else {
+    tp.dataset.stars = '4,5';
+  }
 
-  if (cfg.language) { tp.dataset.language = cfg.language; }
-  { tp.dataset.language = 'en'; }
+  if (cfg.language) {
+    tp.dataset.language = cfg.language;
+  } else {
+    tp.dataset.language = 'en';
+  }
 
-  if (cfg.locale) { tp.dataset.locale = cfg.locale; }
-  else { tp.dataset.locale = 'en-US'; }
+  if (cfg.locale) {
+    tp.dataset.locale = cfg.locale;
+  } else {
+    tp.dataset.locale = 'en-US';
+  }
 
   block.append(tp);
   decorateIcons(block);
