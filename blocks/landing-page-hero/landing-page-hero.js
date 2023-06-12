622541f627d887de34855adbc6025a34feb4a26c
David Granberg <dgranber@adobe.com>
2023-06-12 11:38:41 -0700

    Adding background gradient and styled buttons

--- blocks/landing-page-hero/landing-page-hero.js
+++ blocks/landing-page-hero/landing-page-hero.js
@@ -1,15 +1,14 @@
-// import { span } from '../../scripts/scripts.js';
-
-import { div, span } from '../../scripts/scripts.js';
+import { a, div, span } from '../../scripts/scripts.js';
 import { toClassName } from '../../scripts/lib-franklin.js';
 
 function decorateDescription(descriptionContent) {
   const links = descriptionContent.querySelectorAll('li > a');
   const linksArray = Array.from(links);
   let linksRemaining = linksArray.length;
-  for (const link of linksArray) {
-    // For the landing page hero parse | delimited links from
-    // the markdown to allow for title attribute specification
+
+  // For the landing page hero parse | delimited links from
+  // the markdown to allow for title attribute specification
+  linksArray.forEach((link) => {
     if (link.innerText.includes('|')) {
       const [linkText, linkTitle] = link.innerText.split('|');
       link.setAttribute('title', linkTitle);
@@ -19,23 +18,28 @@ function decorateDescription(descriptionContent) {
     if (linksRemaining > 1) {
       const spanDivider = span({ class: 'divider' }, '|');
       link.after(spanDivider);
+      // eslint-disable-next-line no-plusplus
       linksRemaining--;
     }
+  });
+
+  // If the description texts bulleted list contained no links set the styling on bullets to be just plain text
+  if (linksArray.length === 0) {
+    descriptionContent.classList.add('inline-bulleted-list');
   }
-  return descriptionContent;
 }
 
 function createVideoPlayButton(videoContent) {
   const videoUrl = videoContent.innerText;
   const videoPlayButton = document.createElement('div');
   videoPlayButton.innerHTML = `<button class="slim-video__cta" aria-label="Watch the Vonage Business Solutions overview view in a lightbox">
-<span class="play-video">
-<span class="play-video--background"></span>
-</span>
-<span class="play-video">
-<span class="play-video--arrow"></span>
-</span>
-</button>`;
+                                  <span class="play-video">
+                                    <span class="play-video--background"></span>
+                                  </span>
+                                  <span class="play-video">
+                                    <span class="play-video--arrow"></span>
+                                  </span>
+                               </button>`;
 
   videoPlayButton.addEventListener('click', () => {
     const videoOverlay = (document.querySelector('.video-overlay'));
@@ -48,14 +52,14 @@ function createVideoPlayButton(videoContent) {
 function createVideoOverlay() {
   const videoOverlay = div({ class: 'video-wrapper' });
   videoOverlay.innerHTML = `<section class="video-overlay">
-<div class="video-overlay__background"></div>
-<button class="video-overlay__close" aria-label="Close Button">
-<span class="Vlt-icon-close-circle"></span>
- </button>
-<div class="video-overlay__video">
-<iframe allow="accelerometer; autoplay; encrypted-media; gyroscope;picture-in-picture" allowfullscreen="" class="video-overlay__iframe" title="Activate conversations everywhere with Vonage Business Communications" tabindex="0" id="video-iframe"></iframe>
-</div>
-</section>`;
+                              <div class="video-overlay__background"></div>
+                              <button class="video-overlay__close" aria-label="Close Button">
+                                <span class="Vlt-icon-close-circle"></span>
+                              </button>
+                              <div class="video-overlay__video">
+                               <iframe allow="accelerometer; autoplay; encrypted-media; gyroscope;picture-in-picture" allowfullscreen="" class="video-overlay__iframe" title="Activate conversations everywhere with Vonage Business Communications" tabindex="0" id="video-iframe"></iframe>
+                              </div>
+                            </section>`;
 
   const videoCloseButton = videoOverlay.querySelector('.video-overlay__close');
   videoCloseButton.addEventListener('click', () => {
@@ -68,16 +72,57 @@ function createVideoOverlay() {
   return videoOverlay;
 }
 
-function decorateMediaContent(imageContent, videoContent, overlayContent) {
+function assembleMediaContent(imageContent, videoContent, overlayContent, captionContent) {
   const mediaContent = div({ class: 'landing-page-hero-media-content' });
-  mediaContent.appendChild(imageContent);
+  if (imageContent) {
+    mediaContent.appendChild(imageContent);
+  }
+
+  if (captionContent) {
+    mediaContent.appendChild(captionContent);
+  }
+
+  if (overlayContent) {
+    mediaContent.appendChild(overlayContent);
+  }
+
   if (videoContent) {
     mediaContent.appendChild(createVideoPlayButton(videoContent));
   }
+
   mediaContent.appendChild(createVideoOverlay());
   return mediaContent;
 }
+
+function buildOverlay(overlayRawContent) {
+  const overlayContent = div({ class: 'overlay' });
+
+  const overlayImage = overlayRawContent.querySelector(':scope picture');
+  const overlayLink = overlayRawContent.querySelector(':scope li > a');
+
+  overlayRawContent.querySelectorAll(':scope li > ul > li > code').forEach((styleRule) => {
+    overlayContent.style.cssText += styleRule.innerHTML;
+  });
+
+  overlayContent.appendChild(a({ class: 'overlay-link-wrapper', href: overlayLink.href }, overlayImage));
+
+  return overlayContent;
+}
+function decorateCtasContent(ctasContent, stylesList) {
+  stylesList.forEach((style) => {
+    if (style.includes('button')) {
+      ctasContent.classList.add(style);
+    }
+  });
+}
+
 export default function decorate(block) {
+  // If the markdown specifies a background property, need to bubble that up to the section wrapper
+  if (block.classList.contains('purple-gradient-background')) {
+    block.parentElement.parentElement.classList.add('purple-gradient-background');
+    block.classList.remove('purple-gradient-background');
+  }
+
   const heroContainer = div({ class: block.classList });
   // TODO: Potentially add logic so that you can control which side is the descriptor and which is the media via the order
   //  of the class passed to the block? As well as to allow building a header with only one column
@@ -87,24 +132,31 @@ export default function decorate(block) {
   // Iterate through all the divs of the raw block rows and apply the innerText of the first row as a class to the next since that's how we identify the styling of and nature of the content
   const rawDivElems = block.querySelectorAll(':scope div > div');
 
+  // Parse the elements of the markdown taking the first column row as the classname to apply to the content of the second row, then ditch the first row
   for (let i = 0; i < rawDivElems.length - 1; i += 2) {
     rawDivElems[i + 1].classList.add(toClassName(rawDivElems[i].innerText));
     rawDivElems[i].remove();
   }
 
-  const divElems = block.querySelectorAll(':scope div > div');
-  console.log(divElems);
-
   const titleContent = block.querySelector(':scope div.title');
   const descriptionContent = block.querySelector(':scope div.description');
+  decorateDescription(descriptionContent);
+
   const ctasContent = block.querySelector(':scope div.ctas');
+  decorateCtasContent(ctasContent, block.classList);
 
   const imageContent = block.querySelector(':scope div.image');
   const videoContent = block.querySelector(':scope div.video');
-  const overlayContent = block.querySelector(':scope div.overlay');
+
+  const rawOverlayContent = block.querySelector(':scope div.overlay');
+  let overlayContent;
+  if (rawOverlayContent) {
+    overlayContent = buildOverlay(rawOverlayContent);
+  }
+
   const captionContent = block.querySelector(':scope div.caption');
 
-  heroContainer.querySelector('.category').appendChild(div({ class: 'landing-page-hero-category-content' }, titleContent, decorateDescription(descriptionContent), ctasContent));
-  heroContainer.querySelector('.media').appendChild(decorateMediaContent(imageContent, videoContent, overlayContent));
+  heroContainer.querySelector('.category').appendChild(div({ class: 'landing-page-hero-category-content' }, titleContent, descriptionContent, ctasContent));
+  heroContainer.querySelector('.media').appendChild(assembleMediaContent(imageContent, videoContent, overlayContent, captionContent));
   block.parentElement.replaceChild(heroContainer, block);
 }
