3401d832b6ebbe97597b48749ef9ebd8c76c9c9f
fperez-leappoint <89093701+fperez-leappoint@users.noreply.github.com>
2023-06-13 15:20:31 -0400

    Linting issues
    Fixing any errors

--- blocks/qanda/qanda.js
+++ blocks/qanda/qanda.js
@@ -1,5 +1,5 @@
 function expand(event) {
-  const label = event.target;//.closest('label');
+  const label = event.target;
   const answer = label.nextElementSibling;
   if (label.ariaExpanded === 'true') {
     answer.style.height = 0;
@@ -15,27 +15,27 @@ function expand(event) {
 }
 
 export default function decorate(block) {
-    const maindiv = document.createElement('div');
-    maindiv.className = 'qanda-content';
+  const maindiv = document.createElement('div');
+  maindiv.className = 'qanda-content';
 
-    [...block.children].forEach((row) => {
-        const itemdiv = document.createElement('div');
-        const questionlabel = document.createElement('label');
-        const answerdiv = document.createElement('div');
-        const question = row.getElementsByTagName('div')[0].innerHTML;
-        const answer = row.getElementsByTagName('div')[1].innerHTML;
-        questionlabel.innerHTML = '<span>Q. </span>' + question;
-        answerdiv.innerHTML = '<span>A. </span>' + answer;
-        questionlabel.addEventListener('click', expand);
-        answerdiv.style.height = 0;
-        answerdiv.style.padding = '0 2rem 0';
-        answerdiv.ariaHidden = true;
-        questionlabel.ariaExpanded = false;
-        itemdiv.append(questionlabel);
-        itemdiv.append(answerdiv);
-        maindiv.append(itemdiv);
-    });
+  [...block.children].forEach((row) => {
+    const itemdiv = document.createElement('div');
+    const questionlabel = document.createElement('label');
+    const answerdiv = document.createElement('div');
+    const question = row.getElementsByTagName('div')[0].innerHTML;
+    const answer = row.getElementsByTagName('div')[1].innerHTML;
+    questionlabel.innerHTML = `<span>Q. </span> ${question}`;
+    answerdiv.innerHTML = `<span>A. </span> ${answer}`;
+    questionlabel.addEventListener('click', expand);
+    answerdiv.style.height = 0;
+    answerdiv.style.padding = '0 2rem 0';
+    answerdiv.ariaHidden = true;
+    questionlabel.ariaExpanded = false;
+    itemdiv.append(questionlabel);
+    itemdiv.append(answerdiv);
+    maindiv.append(itemdiv);
+  });
 
-    block.textContent = '';
-    block.append(maindiv);
+  block.textContent = '';
+  block.append(maindiv);
 }
