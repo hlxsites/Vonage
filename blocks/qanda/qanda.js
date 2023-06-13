04fb219c383b81c0af5249c6d1e05d15b0d3cbcf
fperez-leappoint <89093701+fperez-leappoint@users.noreply.github.com>
2023-06-13 17:07:41 -0400

    Added condition for 'Q' and 'A' styling
    Noticed this changed on the site, if a css class alternate is specified, then those labels are added, if not, the contents remain the same

--- blocks/qanda/qanda.js
+++ blocks/qanda/qanda.js
@@ -24,8 +24,13 @@ export default function decorate(block) {
     const answerdiv = document.createElement('div');
     const question = row.getElementsByTagName('div')[0].innerHTML;
     const answer = row.getElementsByTagName('div')[1].innerHTML;
-    questionlabel.innerHTML = `<span>Q. </span> ${question}`;
-    answerdiv.innerHTML = `<span>A. </span> ${answer}`;
+    if (block.classList.contains('alternate')) {
+      questionlabel.innerHTML = `<span>Q. </span> ${question}`;
+      answerdiv.innerHTML = `<span>A. </span> ${answer}`;
+    } else {
+      questionlabel.innerHTML = question;
+      answerdiv.innerHTML = answer;
+    }
     questionlabel.addEventListener('click', expand);
     answerdiv.style.height = 0;
     answerdiv.style.padding = '0 2rem 0';
