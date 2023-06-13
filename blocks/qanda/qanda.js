32a6827f679f751a8a4cc6fcc5bcc9359e9d33d2
fperez-leappoint <89093701+fperez-leappoint@users.noreply.github.com>
2023-06-13 12:51:58 -0400

    Adding animation and expansion

--- blocks/qanda/qanda.js
+++ blocks/qanda/qanda.js
@@ -1,3 +1,18 @@
+function expand(event) {
+  const label = event.target;//.closest('label');
+  const answer = label.nextElementSibling;
+  if (label.ariaExpanded === 'true') {
+    answer.style.height = 0;
+    answer.style.padding = 0;
+    answer.ariaHidden = true;
+    label.ariaExpanded = false;
+  } else {
+    answer.style.height = `${answer.scrollHeight}px`;
+    answer.style.padding = null;
+    answer.ariaHidden = false;
+    label.ariaExpanded = true;
+  }
+}
 
 export default function decorate(block) {
     const maindiv = document.createElement('div');
@@ -11,6 +26,11 @@ export default function decorate(block) {
         const answer = row.getElementsByTagName('div')[1].innerHTML;
         questionlabel.innerHTML = '<span>Q. </span>' + question;
         answerdiv.innerHTML = '<span>A. </span>' + answer;
+        questionlabel.addEventListener('click', expand);
+        answerdiv.style.height = 0;
+        answerdiv.style.padding = 0;
+        answerdiv.ariaHidden = true;
+        questionlabel.ariaExpanded = false;
         itemdiv.append(questionlabel);
         itemdiv.append(answerdiv);
         maindiv.append(itemdiv);
