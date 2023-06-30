6e725921689ccc1ecf60f951f7d0c4049d63bed9
david-granberg <126118840+david-granberg@users.noreply.github.com>
2023-06-30 13:48:28 -0700

    Fixing up margin on the 2-col fragment (#118)

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -47,6 +47,8 @@ export default function decorate(block) {
           // picture is only content in column
           picWrapper.classList.add('columns-img-col');
         }
+      } else {
+        col.classList.add('columns-other-col');
       }
     });
   });
