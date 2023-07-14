be86b788389f1e77396038d80a9e22c6bfc2a9c2
Aravindo Wingeier <wingeier@adobe.com>
2023-07-14 12:16:06 -0700

    first poc

--- blocks/quote-form/quote-form.js
+++ blocks/quote-form/quote-form.js
@@ -1,3 +1,42 @@
-export default async function decorate(block) {
+import { div } from '../../scripts/scripts.js';
 
+export default async function decorate(block) {
+  const card = block.querySelector(':scope > div > div:first-child');
+  card.classList.add('card');
+  await decorateCard(card);
+
+  const form = block.querySelector(':scope > div > div:last-child');
+  form.classList.add('form-wrapper');
+
+  await decorateForm(form);
+}
+
+async function decorateCard(card) {
+  // remove button containers
+  card.querySelectorAll('.button-container').forEach((container) => {
+    [...container.children].forEach((node) => {
+      node.classList.remove('button');
+      node.classList.remove('primary');
+    });
+    container.classList.remove('button-container');
+  });
+}
+
+async function decorateForm(formWrapper) {
+  const htmlFile = formWrapper.querySelector('p').textContent;
+  formWrapper.querySelector('p').remove();
+  formWrapper.querySelector('hr').remove();
+
+  const thankYouMesage = formWrapper.children;
+  formWrapper.textContent = '';
+
+  const resp = await fetch(`/blocks/quote-form/${htmlFile}`);
+  if (resp.ok) {
+    const form = div({ class: 'form' });
+    formWrapper.append(form);
+    form.innerHTML = await resp.text();
+  } else {
+    // eslint-disable-next-line no-console
+    console.warn(`File not found: ${htmlFile} - can not render form`);
+  }
 }
