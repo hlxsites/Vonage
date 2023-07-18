0f70842cc4614388314a4cbcdbd2a33c204d19f7
David Granberg <dgranber@adobe.com>
2023-07-18 14:56:49 -0700

    Jump link block

---
+++ blocks/jump-links/jump-links.js
@@ -0,0 +1,30 @@
+import { div } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`jump-links-${cols.length}-cols`);
+
+  const linkTexts = [...block.querySelectorAll('.jump-links h2')];
+  const linkTargets = [...block.querySelectorAll('.jump-links a')];
+
+  block.innerHTML = `
+    <div class="jump-links-spacer "></div>
+    <div class="container">
+        <div class="row"></div>
+    </div>`;
+
+  linkTexts.forEach((linkText, index) => {
+    const column = div({ class: 'column' });
+    column.innerHTML = `<div class="jump-links-jump-link">
+    <a class="jump-links-link" href="javascript:void(0)" data-target="${linkTargets[index].getAttribute('href')}"></a>
+    <div class="jump-links-heading">
+      <div class="jump-links-icon">
+      </div>
+      <h2 class="jump-links-title">${linkText.innerText}</h2>
+      <span class="vlt-icon-arrow-link" aria-hidden="true"></span>
+    </div>
+    <span class="jump-links-label"></span>`;
+
+    block.querySelector('div.row').appendChild(column);
+  });
+}
